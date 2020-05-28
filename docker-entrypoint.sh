f901e2e97618467c2fd3279ad716b0fea492ff61
Rifqi Al Fatih <rifqialfatih@gmail.com>
2020-05-29 00:21:13 +0200

    Add supervisord config remove env HBP eval

---
+++ docker-entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get public IP
+export NODE_IP=$(curl -H "Metadata-Flavor: Google" http://169.254.169.254/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+
+echo "127.0.0.1 $(uname -n)" | sudo tee --append /etc/hosts
+sudo git clone --progress --branch=master18 https://bitbucket.org/hbpneurorobotics/Models.git Models/
+sudo git clone --progress --branch=master18 https://bitbucket.org/hbpneurorobotics/Experiments.git Experiments/
+sudo chown -R bbpnrsoa:bbp-ext /home/bbpnrsoa/nrp/src/Experiments && sudo chown -R bbpnrsoa:bbp-ext /home/bbpnrsoa/nrp/src/Models
+/home/bbpnrsoa/nrp/src/user-scripts/rendering_mode cpu
+pip install pillow && python /home/bbpnrsoa/nrp/src/user-scripts/generatelowrespbr.py
+export NRP_MODELS_DIRECTORY=$HBP/Models && /home/bbpnrsoa/nrp/src/Models/create-symlinks.sh
+/bin/sed -e 's/localhost/'"$NODE_IP"'/' -i /home/bbpnrsoa/nrp/src/ExDBackend/hbp_nrp_commons/hbp_nrp_commons/workspace/Settings.py
+/bin/sed -e 's/localhost/'"$NODE_IP"'/' -i /home/bbpnrsoa/nrp/src/VirtualCoach/hbp_nrp_virtual_coach/hbp_nrp_virtual_coach/config.json
+cd $HOME/nrp/src && . $HOME/.opt/platform_venv/bin/activate && pyxbgen -u Experiments/bibi_configuration.xsd -m bibi_api_gen && pyxbgen -u Experiments/ExDConfFile.xsd -m exp_conf_api_gen && pyxbgen -u Models/environment_model_configuration.xsd -m environment_conf_api_gen && pyxbgen -u Models/robot_model_configuration.xsd -m robot_conf_api_gen && deactivate
+gen_file_path=$HBP/ExDBackend/hbp_nrp_commons/hbp_nrp_commons/generated && filepaths=$HOME/nrp/src && sudo cp $filepaths/bibi_api_gen.py $gen_file_path &&  sudo cp $filepaths/exp_conf_api_gen.py $gen_file_path && sudo cp $filepaths/_sc.py $gen_file_path && sudo cp $filepaths/robot_conf_api_gen.py $gen_file_path && sudo cp $filepaths/environment_conf_api_gen.py $gen_file_path
+
+sudo /usr/bin/supervisord -n
