c906ff00e402578638a8f78aa802ba8d6b598318
Rifqi Al Fatih <rifqialfatih@gmail.com>
2020-06-04 11:13:29 +0200

    Add volume, fix host port

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,9 +10,15 @@ sudo chown -R bbpnrsoa:bbp-ext /home/bbpnrsoa/nrp/src/Experiments && sudo chown 
 /home/bbpnrsoa/nrp/src/user-scripts/rendering_mode cpu
 pip install pillow && python /home/bbpnrsoa/nrp/src/user-scripts/generatelowrespbr.py
 export NRP_MODELS_DIRECTORY=$HBP/Models && /home/bbpnrsoa/nrp/src/Models/create-symlinks.sh
-/bin/sed -e 's/localhost:9000/'"$NODE_IP"':30000/' -i /home/bbpnrsoa/nrp/src/ExDBackend/hbp_nrp_commons/hbp_nrp_commons/workspace/Settings.py
-/bin/sed -e 's/localhost:9000/'"$NODE_IP"':30000/' -i /home/bbpnrsoa/nrp/src/VirtualCoach/hbp_nrp_virtual_coach/hbp_nrp_virtual_coach/config.json
 cd $HOME/nrp/src && . $HOME/.opt/platform_venv/bin/activate && pyxbgen -u Experiments/bibi_configuration.xsd -m bibi_api_gen && pyxbgen -u Experiments/ExDConfFile.xsd -m exp_conf_api_gen && pyxbgen -u Models/environment_model_configuration.xsd -m environment_conf_api_gen && pyxbgen -u Models/robot_model_configuration.xsd -m robot_conf_api_gen && deactivate
 gen_file_path=$HBP/ExDBackend/hbp_nrp_commons/hbp_nrp_commons/generated && filepaths=$HOME/nrp/src && sudo cp $filepaths/bibi_api_gen.py $gen_file_path &&  sudo cp $filepaths/exp_conf_api_gen.py $gen_file_path && sudo cp $filepaths/_sc.py $gen_file_path && sudo cp $filepaths/robot_conf_api_gen.py $gen_file_path && sudo cp $filepaths/environment_conf_api_gen.py $gen_file_path
+
+# For kubernetes
+/bin/sed -e 's/localhost:9000/'"$NODE_IP"':30000/' -i /home/bbpnrsoa/nrp/src/ExDBackend/hbp_nrp_commons/hbp_nrp_commons/workspace/Settings.py
+/bin/sed -e 's/localhost:9000/'"$NODE_IP"':30000/' -i /home/bbpnrsoa/nrp/src/VirtualCoach/hbp_nrp_virtual_coach/hbp_nrp_virtual_coach/config.json
+
+# For local docker
+# /bin/sed -e 's/localhost:9000/host.docker.internal:9000/' -i /home/bbpnrsoa/nrp/src/ExDBackend/hbp_nrp_commons/hbp_nrp_commons/workspace/Settings.py
+# /bin/sed -e 's/localhost:9000/host.docker.internal:9000/' -i /home/bbpnrsoa/nrp/src/VirtualCoach/hbp_nrp_virtual_coach/hbp_nrp_virtual_coach/config.json
 
 sudo ROS_IP=$(hostname -I | cut -d " " -f 1) HBP=/home/bbpnrsoa/nrp/src /usr/bin/supervisord -n
